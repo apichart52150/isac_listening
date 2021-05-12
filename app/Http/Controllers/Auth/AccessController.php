dcb075e4089f28a28fc34631280c642a8daadcdc
Apichart Partoommart <apichart.p@newcambridge.net>
2021-05-12 12:15:43 +0700

    commit

--- app/Http/Controllers/Auth/AccessController.php
+++ app/Http/Controllers/Auth/AccessController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Model\Ipack;
+use App\Ipack;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
