09800192e09b56674dcbff594cea5227df9c6dac
Apichart Partoommart <apichart.p@newcambridge.net>
2021-05-14 09:41:00 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,9 +21,19 @@
 @section('content')
 
 <style>
-    .icons-list-demo i {
-        width: 10px!important;
+    .hover-scale i.fas {
+        transform: scale(1);
+        transition: transform .15s ease;
     }
+
+    .hover-scale:hover i.fas {
+        transform: scale(1.1);
+    }
+
+    .border-2 {
+        border-width: 2px;
+    }
+
 </style>
 <!-- start page title -->
 <div class="row">
@@ -46,36 +56,40 @@
 
     <div class="col-sm-6 col-xl-3 filter-item all web illustrator">
         <div class="gal-box">
-            <a href="#" class="btn btn-info waves-effect waves-light w-100 p-3">
-                <img class="img-fluid" src="{{ asset('public/assets/images/sac-01.png') }}">
+            <a href="javascript: void(0);" class="hover-scale" id="general-english">
+                <div class="widget-simple text-center w-100 p-3 bg-info">
+                    <i class="fas fa-headphones text-light fa-8x mb-2"></i>
+                </div>
             </a>
             <div class="gall-info"  id="headingOne">
                 <h4 class="font-16 mt-0 text-info">Challenge Practice</h4>
-
                 <div class="accordion" id="accordionExample1">
                     <a class="collapsed gal-like-btn" data-toggle="collapse" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                        <i class="ti-angle-down text-info"></i>
-                        
+                        <div class="hover-scale">
+                            <div class="widget-simple">
+                                <i class="fas fa-angle-down text-info fa-2x"></i>
+                            </div>
+                        </div>
                     </a>
                     <img src="{{ asset('public/assets/images/file-icons/pdf.svg') }}" alt="user-img" class="rounded-circle" height="24" />
                     <span class="text-muted ml-1">Download PDF.file</span>
                     <div id="collapseOne" class="collapse mt-2" aria-labelledby="headingOne" data-parent="#accordionExample1">
                         <div class="row icons-list-demo">
-                            <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                            <div class="col-lg-12" >
+                                <a href="#"  class="text-info">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Challenges-Practice-Advanced
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#"  class="text-info">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Challenges-Practice-Business
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#"  class="text-info">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Challenges-Practice-Intermediate
                                 </a>
                             </div>
@@ -89,41 +103,46 @@
 
     <div class="col-sm-6 col-xl-3 filter-item all web illustrator">
         <div class="gal-box">
-            <a href="#" class="btn btn-warning waves-effect waves-light w-100 p-3">
-                <img src="{{ asset('public/assets/images/sac-02.png') }}" class="img-fluid" alt="work-thumbnail">
+            <a href="javascript: void(0);" class="hover-scale" id="general-english">
+                <div class="widget-simple text-center w-100 p-3 bg-warning">
+                    <i class="fas fa-headphones text-light fa-8x mb-2"></i>
+                </div>
             </a>
             <div class="gall-info">
                 <h4 class="font-16 mt-0 text-warning">NC Listening Test</h4>
                 <div class="accordion" id="accordionExample2">
                     <a class="collapsed gal-like-btn" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        <i class="ti-angle-down text-warning"></i>
-                        
+                        <div class="hover-scale">
+                            <div class="widget-simple">
+                                <i class="fas fa-angle-down text-warning fa-2x"></i>
+                            </div>
+                        </div>
                     </a>
                     <img src="{{ asset('public/assets/images/file-icons/pdf.svg') }}" alt="user-img" class="rounded-circle" height="24" />
                     <span class="text-muted ml-1">Download PDF.file</span>
                     <div id="collapseTwo" class="collapse mt-2" aria-labelledby="headingTwo" data-parent="#accordionExample2">
                         <div class="row icons-list-demo">
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-warning">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     NC-Listening-Test-I
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-warning">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     NC-Listening-Test-II
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-warning">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     NC-Listening-Test-III
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-warning">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     NC-Listening-Test-IV
                                 </a>
                             </div>
@@ -137,77 +156,82 @@
 
     <div class="col-sm-6 col-xl-3 filter-item all web illustrator">
         <div class="gal-box">
-            <a href="#" class="btn btn-danger waves-effect waves-light w-100 p-3">
-                <img src="{{ asset('public/assets/images/sac-03.png') }}" class="img-fluid" alt="work-thumbnail">
+            <a href="javascript: void(0);" class="hover-scale" id="general-english">
+                <div class="widget-simple text-center w-100 p-3 bg-danger">
+                    <i class="fas fa-headphones text-light fa-8x mb-2"></i>
+                </div>
             </a>
             <div class="gall-info">
                 <h4 class="font-16 mt-0 text-danger">Active Listening</h4>
                 <div class="accordion" id="accordionExample3">
                     <a class="collapsed gal-like-btn" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                        <i class="ti-angle-down text-danger"></i>
-                        
+                        <div class="hover-scale">
+                            <div class="widget-simple">
+                                <i class="fas fa-angle-down text-danger fa-2x"></i>
+                            </div>
+                        </div>
                     </a>
                     <img src="{{ asset('public/assets/images/file-icons/pdf.svg') }}" alt="user-img" class="rounded-circle" height="24" />
                     <span class="text-muted ml-1">Download PDF.file</span>
                     <div id="collapseThree" class="collapse mt-2" aria-labelledby="headingThree" data-parent="#accordionExample3">
                         <div class="row icons-list-demo">
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Crime and punishent
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Education
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Enviroment
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     FestivalsAndTraditions
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     FloraAndFauna
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Food
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Health Care
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Homes
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Sports and Pastimes
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-danger">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     StrangePhenomena
                                 </a>
                             </div>
@@ -221,35 +245,40 @@
 
     <div class="col-sm-6 col-xl-3 filter-item all web illustrator">
         <div class="gal-box">
-            <a href="#" class="btn btn-pink waves-effect waves-light w-100 p-3">
-                <img src="{{ asset('public/assets/images/sac-04.png') }}" class="img-fluid" alt="work-thumbnail">
+            <a href="javascript: void(0);" class="hover-scale" id="general-english">
+                <div class="widget-simple text-center w-100 p-3 bg-pink">
+                    <i class="fas fa-headphones text-light fa-8x mb-2"></i>
+                </div>
             </a>
             <div class="gall-info">
                 <h4 class="font-16 mt-0 text-pink">Language Drills Practice</h4>
                 <div class="accordion" id="accordionExample4">
                     <a class="collapsed gal-like-btn" data-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                        <i class="ti-angle-down text-pink"></i>
-                        
+                        <div class="hover-scale">
+                            <div class="widget-simple">
+                                <i class="fas fa-angle-down text-pink fa-2x"></i>
+                            </div>
+                        </div>
                     </a>
                     <img src="{{ asset('public/assets/images/file-icons/pdf.svg') }}" alt="user-img" class="rounded-circle" height="24" />
                     <span class="text-muted ml-1">Download PDF.file</span>
                     <div id="collapseFour" class="collapse mt-2" aria-labelledby="headingFour" data-parent="#accordionExample4">
                         <div class="row icons-list-demo">
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-pink">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Language-Drills-Elementary
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-pink">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Language-Drills-Intermediate
                                 </a>
                             </div>
                             <div class="col-lg-12">
-                                <a href="#">
-                                    <i class="fe-download" style="font-size: 18px"></i>
+                                <a href="#" class="text-pink">
+                                    <i class="fas fa-download fa-2x" style="font-size: 18px"></i>
                                     Language-Drills-Advanced
                                 </a>
                             </div>
